2c067e955304fc1d7b611532c0d8635e7acc9c4d
Sandeep <sandeep@codewithsandeep.com>
2021-12-14 16:58:20 +0100

    Anregungs-, Doppler- und Mixedsignale und dazugehörigen Diagramme

---
+++ Hausaufgabe_Doppler.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+E = 1
+N = 10000
+ft = 1
+fd= 0.9
+T = 0.006
+p1 = 0.5
+p2 = 0.5
+
+t = np.linspace(0, T*N, N)
+uSig = E*np.sin(2*np.pi*ft*t)
+zss = E*p1*np.sin(2*np.pi*ft*t)
+zsb = E*p2*np.sin(2*np.pi*fd*t)
+bs = zss + zsb
+mix = bs*np.sin(2*np.pi*ft*t)
+
+def Anregungswelle(sig,T, title='',mitOrig='no'):
+    fourier=np.fft.rfft(sig)
+    ampli=np.abs(fourier)
+#    anzahlFrequenzen=len(ampli)
+#    print(anzahlFrequenzen)
+    xWerteFreq=np.linspace(0, 1/(2*T), N//2 +1)
+    
+    if mitOrig == 'yes':
+        plt.figure()
+        plt.subplot(2,1,1)
+        plt.title(title)
+        plt.plot(t,sig)
+        plt.subplot(2,1,2)
+        plt.plot(xWerteFreq,ampli)
+        plt.show() 
+        
+    else:
+        plt.figure()
+        plt.title(title)
+        plt.plot(t,sig)
+        plt.show() 
+        
+    
+
+    
+    
+   
+        
+        
+        
+    
+   
---
+++ Importing_Hausaufgabe_Doppler.py
@@ -0,0 +1,99 @@
+from Hausaufgabe_Schie import *
+
+
+
+
+
+#sig = uSig
+Anregungswelle (uSig, T, 'Anregungswelle des Wandlers')
+
+
+#sig = zss
+Anregungswelle (zss, T, 'zurückkommende signal von stationärer quelle')
+
+#sig = bs
+Anregungswelle (bs, T, 'dopplersignal: zurückkommende signal von beweglicher quelle')
+
+
+#sig = mix
+Anregungswelle (mix, T, 'Gemischte Signal von Detektor', mitOrig='yes')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#sig1= str(input('Bitte geben Sie gewünschte signale ein: '))
+
+#if sig1 == 'uSig':
+#    plt.title(title)
+#    plt.plot(t,sig)
+#    plt.show() 
+#elif sig1 == 'bs':
+#    plt.title(title)
+#    plt.plot(t,sig)
+#    plt.show()
+#    
+#elif sig1 == 'zss':
+#    plt.title(title)
+#    plt.plot(t,sig)
+#    plt.show()
+#
+#elif sig1 == 'mix':
+#    plt.figure()
+#    plt.subplot(2,1,1)
+#    plt.title(title)
+#    plt.plot(t,sig)
+#    plt.subplot(2,1,2)
+#    plt.plot(xWerteFreq,ampli)
+#    plt.show()
+#    
+    
+    
+    
+    
+    
+    
+    
+         
+         
